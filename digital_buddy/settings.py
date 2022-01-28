b64057f76023878e25461a558b873f295dfc70f9
Kiran <kgawas9@gmail.com>
2022-01-29 00:31:59 +0530

    First commit

--- digital_buddy/settings.py
+++ digital_buddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-orbvkth%_8-at@6rx+(6%gh-d0usass=bluz++rci7mr)xdz3g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
